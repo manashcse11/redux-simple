935a359df06eab969bc56b467468f78f3578d532
Manash <manash@cefalo.com>
2020-09-02 09:46:01 +0600

    Get & update data from redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import { connect } from 'react-redux';
 import { addUserData } from './redux/actions';
 
 const App = ({ user, addUserData }) => {
+  const [name, setName] = useState(user != null ? (user.hasOwnProperty('name') ? user.name : '') : '');
+  const [profession, setProfession] = useState(user != null ? (user.hasOwnProperty('profession') ? user.profession : '') : '');
+
+  const handleChange = (event) => {
+    if(event.target.name == "name"){
+      setName(event.target.value);
+    }
+    if(event.target.name == "profession"){
+      setProfession(event.target.value);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    addUserData({name, profession});
+  };
+
   return (
     <div className="App">
-      <h1>React Redux</h1>
+      <h1>React Redux Example</h1>
+      Name: <input type="text" name="name" value={name} onChange={handleChange} /><br/>
+      Profession: <input type="text" name="profession" value={profession} onChange={handleChange} /><br/>
+      <button type="submit" onClick={handleSubmit}>Update</button>
     </div>
   );
 }
